9848c91fbced27d7db6b9d0d521f553ddd257b49
Lihuen Figueroa <lfigueroa.ext@prestige-auto.com.ar>
2026-07-22 07:08:17 +0000

    wp-theme: integra Google Analytics 4 y Meta Pixel en header.php

--- wp-theme/smart-argentina/header.php
+++ wp-theme/smart-argentina/header.php
@@ -12,5 +12,32 @@
     window.WP_CONTACT_NONCE = '<?php echo wp_create_nonce('smart_contacto'); ?>';
     window.WP_GRACIAS_URL  = '<?php echo home_url('/gracias/'); ?>';
   </script>
+
+  <!-- Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-NGZTK3NT0Q"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-NGZTK3NT0Q');
+  </script>
+
+  <!-- Meta Pixel Code -->
+  <script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1521748305849211');
+  fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1521748305849211&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Meta Pixel Code -->
 </head>
 <body <?php body_class('bg-white font-smart-next'); ?>>
